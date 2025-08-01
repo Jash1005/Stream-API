f8605aee6d24b4f0f539bdb1a5694822bb12b5d2
Jash1005 <jasheth2004@gmail.com>
2025-08-01 18:11:35 +0530

    Create forEach.java

---
+++ forEach.java
@@ -0,0 +1,33 @@
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        // Supplier is an interface that supplies data
+        Supplier<String> supplier = new Supplier<>() {
+            @Override
+            public String get() {
+                return "hello stream!!";
+            }
+        };
+
+        // Consumer is an interface that consumes data
+        Consumer<String> consumer = new Consumer<>() {
+            @Override
+            public void accept(String str) {
+                System.out.println(str);
+            }
+        };
+
+        // Create an infinite stream from the supplier
+        // generate keeps producing data which are streamed in data pipe
+        Stream<String> streamOfStrings = Stream.generate(supplier);
+
+        // Limit the stream to prevent infinite loop
+        streamOfStrings
+            .limit(5) // limit to 5 items
+            .forEach(consumer);
+    }
+}
