196e3652efd62e8a8e2a313114ae66d63b7e6080
Jash1005 <jasheth2004@gmail.com>
2025-09-03 23:24:31 +0530

    Update method_reference_2.java

--- method_reference_2.java
+++ method_reference_2.java
@@ -59,3 +59,64 @@ class Main {
         return a + b;
     }
 }
+
+
+
+class Main {
+    
+    private interface Iadd {
+        int sum (int a,int b);
+    }
+    
+    public int addtwonum(int a,int b) {
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+        // diff btw direct refernce or method calling and method reference
+        
+        Main m = new Main();
+        int a = m.addtwonum(10,10); // this means at this position of code i want my output to be done
+        System.out.println(a);
+        
+        Iadd iadd = m::addtwonum;
+        // here i can anytime invoke this method
+        
+        System.out.println(iadd.sum(1,2));
+        
+    }
+}
+
+
+// calling by class name
+
+class Main {
+    
+    private interface Iadd {
+        int sum (int a,int b);
+    }
+    
+    private interface Iadd2 {
+        int sum (Main sampleobj , int a , int b);
+    }
+    
+    public int addtwonum(int a,int b) {
+        return a + b;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        Iadd iadd = new Main()::addtwonum;
+        // we can use instance of obj 
+        
+        // Iadd iadd = Main::addtwonum; // addtwonum should be static here
+                // Main is class name but we wont instance of obj
+       
+        Iadd2 iadd2 = Main::addtwonum; // new Main().addtwonum() 
+        // the obj we pass will replace class name and it will work as object
+        int sum = iadd2.sum(new Main() , 2 , 5);
+        System.out.println(sum);
+        
+    }
+}
